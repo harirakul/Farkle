f11444d145c89f1403a1301dd64a64b8233a0138
Hari <harirakul.a@gmail.com>
2023-03-07 11:38:16 -0800

    Fixed im selection; Storing dice values

--- DiceArray.m
+++ DiceArray.m
@@ -11,22 +11,56 @@ classdef DiceArray
             end
         end
 
-        function obj = rollSelected(obj)
-            rolls = randi(6, 1, 6);
+
+        % function obj = rollSelected(obj)
+        %     rolls = randi(6, 1, 6);
+        %     for i = 1:6
+        %         if obj.dice(i).selected == true
+        %             obj.dice(i).value = rolls(i);
+        %         end
+        %     end
+        % end
+
+        % Select or unselect a die at the specified index accordingly.
+        function obj = changeSelection(obj, idx)
+            obj.dice(idx).selected = ~obj.dice(idx).selected;
+        end
+
+        % Return a logical value if the die at the input idx is selected
+        function L = selected(obj, idx)
+            L = obj.dice(idx).selected;
+        end
+
+        % Set dice values to values in valueArr
+        function obj = updateValues(obj, valueArr)
             for i = 1:6
-                if obj.dice(i).selected == true
-                    obj.dice(i).value = rolls(i);
+                obj.dice(i).value = valueArr(i);
+            end
+        end
+
+        % Return an array of values that are selected
+        function arr = selectedValues(obj)
+            for i = 1:6
+                if (obj.dice(i).selected)
+                    arr = [arr, obj.dice(i).value];
                 end
             end
         end
 
-        function obj = select(obj, idx)
-            obj.dice(idx).selected = true;
-        end
+        function arr = allValues(obj)
+            for i = 1:6
+                arr = [arr, obj.dice(i).value];
+            end
+        end  
 
-        function obj = deselect(obj, idx)
-            obj.dice(idx).selected = false;
-        end
-
+        % Generate melds.
+        function arr = generateMelds(obj)
+            vals = obj.allValues();
+            counts = [];
+            for i = 1:6
+                counts = [counts, numel(find(vals==i))];
+            end
+            % To Do: finish
+        end  
     end
 end

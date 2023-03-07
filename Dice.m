a4a95fa82e41853dc1769ee4ba9575c3d3ab6a28
Hari <harirakul.a@gmail.com>
2023-03-06 20:45:07 -0800

    Removed bugs

--- Dice.m
+++ Dice.m
@@ -1,21 +1,23 @@
 classdef Dice
     properties
-        value
+        value        numeric
+        selected     logical
     end
     methods
         function dice = Dice()
-            dice.value = randi([1, 6]);
+            obj.value = randi([1, 6]);
         end
-        function rolldice = Dice(dice)
-            dice.value = randi([1, 6]);
-        end
-        function score = comparison(multipledice)
-            number_of_dice = numel(multipledice);
-            score = zeros(multipledice, multipledice);
-            for (i = 1:number_of_dice)
-                for (j = i+1:number_of_dice)
-                    if (multipledice()
 
-            end
+        function rolldice()
+            obj.value = randi([1, 6]);
         end
+
+        % function score = comparison(multipledice)
+        %     number_of_dice = numel(multipledice);
+        %     score = zeros(multipledice, multipledice);
+        %     for (i = 1:number_of_dice)
+        %         for (j = i+1:number_of_dice)
+        %             if (multipledice()
+
+    end
 end

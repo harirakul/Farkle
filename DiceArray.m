f8c2331eccdc4507a7a7b6245c44a529e7db84d4
Hari <harirakul.a@gmail.com>
2023-03-17 12:12:12 -0700

    Manipulating score for quad and quintuples

--- DiceArray.m
+++ DiceArray.m
@@ -32,15 +32,6 @@ classdef DiceArray
             end
         end
 
-        % function obj = rollSelected(obj)
-        %     rolls = randi(6, 1, 6);
-        %     for i = 1:6
-        %         if obj.dice(i).selected == true
-        %             obj.dice(i).value = rolls(i);
-        %         end
-        %     end
-        % end
-
         % Select or unselect a die at the specified index accordingly.
         function obj = changeSelection(obj, idx)
             obj.dice(idx).selected = ~obj.dice(idx).selected;
@@ -105,10 +96,30 @@ classdef DiceArray
             end
 
             initialCounts = counts;
-            triples = find(counts >= 3);
+            % Deal with quadruple, quintuple, and 6-of-a-kind
 
-            %Deal with combos:
-            while numel(counts(counts >= 3)) > 0
+            sixes = find(counts == 6);
+            for i = 1:length(sixes)
+                score = score + 3500;
+                counts(sixes(i)) = counts(sixes(i)) - 6;
+            end
+
+            quints = find(counts == 5);
+            for i = 1:length(quints)
+                score = score + 2000;
+                counts(quints(i)) = counts(quints(i)) - 5;
+            end
+
+            quads = find(counts == 4);
+            for i = 1:length(quads)
+                score = score + 1000;
+                counts(quads(i)) = counts(quads(i)) - 4;
+            end
+            
+            triples = find(counts == 3);
+
+            %Deal with triple combos:
+            while numel(counts(counts == 3)) > 0
                 idxs = find(counts >= 3); %Indexes where there are combos
 
                 % Update combo scores
@@ -127,9 +138,6 @@ classdef DiceArray
             numOnes = counts(1);
             score = score + 100*numOnes;
             hasMeld = score > 0;
-%             if scoreP1 >= 1000
-%                 isWinP1 = true;
-%             end
 
         end 
 
